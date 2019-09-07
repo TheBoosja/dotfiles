19f72354a3e5ff5c27470403d4dd9359331d6d0d
TheBoosja <mathias.iversen487@gmail.com>
2019-09-07 23:05:24 +0200

    Added Boosja

---
+++ BALARS.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+############################################
+### Boosja's Arch Linux Auto-Rice Script ###
+############################################
+
+### Get information from user ###
+hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+clear
+: ${hostname:?"hostname cannot be empty"}
+
+user=$(dialog --stdout --inputbox "Enter admin username" 0 0) || exit 1
+clear
+: ${user:?"user cannot be empty"}
+
+password=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
+clear
+: ${password:?"password cannot be empty"}
+password2=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
+clear
+[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1
+clear
+
+fdisk "${device}"
+part_root="${device}1"
+part_home="${device}2"
+mkfs.ext4 "${part_root}"
+mkfs.ext4 "${part_home}"
+
+mount "${part_root}" /mnt
+mkdir /mnt/home
+mount "${part_home}" /mnt/home
+
+arch-chroot /mnt
