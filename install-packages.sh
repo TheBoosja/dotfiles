b56f9721a8376ac661bace94d37cbfcc244f2b87
Boosja <mathias.iversen487@gmail.com>
2022-07-17 15:14:28 +0200

    Load nvm after install

--- install-packages.sh
+++ install-packages.sh
@@ -6,9 +6,19 @@ sudo apt upgrade
 sudo apt install curl
 if type -p curl > /dev/null; then
   echo "curl installed" >> $log_file
+else
+  echo "curl FAILED" >> $log_file
+fi
 
-  # nvm
+
+# Node (nvm)
+if type -p curl > /dev/null; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
   if type -p nvm > /dev/null; then
     echo "nvm installed" >> $log_file
 
@@ -17,8 +27,6 @@ if type -p curl > /dev/null; then
   else
     echo "nvm FAILED" >> $log_file
   fi
-else
-  echo "curl FAILED" >> $log_file
 fi
 
 echo -e "\n======== Summary ========\n"
